eb9d7d4fc50fb20ac1ef7a006705075315f8dfc8
bartox7777 <bartosz.zelek@protonmail.com>
2024-10-18 21:06:08 +0200

    add combiner

--- src/LandingsAndDelays.java
+++ src/LandingsAndDelays.java
@@ -1,7 +1,6 @@
 import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -16,16 +15,17 @@ import org.apache.hadoop.util.ToolRunner;
 import java.io.File;
 import java.io.IOException;
 
-// PYTANIA
+// Q&A
 // 1. Czy brać pod uwagę jedynie dodatnie opóźnienia? TAK MAX(0, ARRIVAL_DELAY)
-// 2. Jak uwzględnić brakujące wartości w kolumnie ARRIVAL_DELAY?
-// 3. Czy format wyjściowy jest poprawny? AMA,2015,12	237,493.0
+// 2. Jak uwzględnić brakujące wartości w kolumnie ARRIVAL_DELAY? - jeżeli CANCELLED=0 to traktuję jako brak opóźnienia -> 0
+// 3. Czy brać pod uwagę kolumnę DIVERTED? - nie
 
 
 public class LandingsAndDelays extends Configured implements Tool{
 
 
-    public static class LandsingsAndDelaysMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    // <offset in file, line from file> -> <"airportID,year,month", delay>
+    public static class LandsingsAndDelaysMapper extends Mapper<LongWritable, Text, Text, Text> {
         private static final int YEAR = 0; // valid 100%
         private static final int MONTH = 1; // valid 100%
         private static final int DESTINATION_AIRPORT = 8; // AIRPORT ID - valid 100%
@@ -37,7 +37,7 @@ public class LandingsAndDelays extends Configured implements Tool{
         private final IntWritable outputValue = new IntWritable();
 
         @Override
-        protected void map(LongWritable offset, Text line, Context context) throws IOException, InterruptedException {
+        public void map(LongWritable offset, Text line, Context context) throws IOException, InterruptedException {
             String[] columns = line.toString().split(",", -1);
             if (columns.length != 31){
                 System.out.println("Invalid record(!=31 records): " + line.toString());
@@ -57,20 +57,42 @@ public class LandingsAndDelays extends Configured implements Tool{
 
             outputKey.set(columns[DESTINATION_AIRPORT] + "," + columns[YEAR] + "," + columns[MONTH]);
             outputValue.set(arrivalDelay);
-            context.write(outputKey, outputValue);
+            context.write(outputKey, new Text("1,"+outputValue));
         }
     }
 
-    public static class LandsingsAndDelaysReducer extends Reducer<Text, IntWritable, Text, Text> {
+    // <"airportID,year,month", "1,delay"> -> <"airportID,year,month", "numberOfLandings,delaysSum">
+    // counting number of landings by incrementing
+    public static class LandingsAndDelaysCombiner extends Reducer<Text, Text, Text, Text>{
         @Override
-        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            double delaysSum = 0;
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+            int delaysSum = 0;
             int numberOfLandings = 0;
-            for (IntWritable value : values) {
-                delaysSum += value.get();
+            for (Text value : values) {
+                String[] valuesArray = value.toString().split(",");
+                delaysSum += Integer.parseInt(valuesArray[1]);
                 numberOfLandings++;
             }
-            context.write(key, new Text(numberOfLandings + "," + delaysSum  ));
+            context.write(key, new Text(numberOfLandings + "," + delaysSum));
+        }
+    }
+
+
+    // <"airportID,year,month", "numberOfLandings,delaysSum"> -> <"airportID,year,month", "numberOfLandings,delaysSum">
+    // counting number of landings by summing counters
+    public static class LandsingsAndDelaysReducer extends Reducer<Text, Text, Text, Text> {
+        @Override
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            int delaysSum = 0;
+            int numberOfLandings = 0;
+
+            for (Text value : values) {
+                String[] valuesArray = value.toString().split(",");
+                numberOfLandings += Integer.parseInt(valuesArray[0]);
+                delaysSum += Integer.parseInt(valuesArray[1]);
+            }
+
+            context.write(key, new Text(numberOfLandings + "," + delaysSum));
         }
     }
 
@@ -81,9 +103,10 @@ public class LandingsAndDelays extends Configured implements Tool{
 
         job.setMapperClass(LandsingsAndDelaysMapper.class);
         job.setReducerClass(LandsingsAndDelaysReducer.class);
+        job.setCombinerClass(LandingsAndDelaysCombiner.class);
 
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
+        job.setMapOutputValueClass(Text.class);
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
@@ -91,7 +114,7 @@ public class LandingsAndDelays extends Configured implements Tool{
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        job.setNumReduceTasks(1);
+        job.setNumReduceTasks(3);
 
         return job.waitForCompletion(true) ? 0 : 1;
     }
